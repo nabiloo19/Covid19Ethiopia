55e5f76042d48087d7610e4e9fc2505bb7570865
nabiloo19 <nabil.alanssi19@gmail.com>
2020-03-23 12:57:36 +0300

    :zap: Update&Offline Mode Show Messages

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -17,13 +17,13 @@ if (process.env.NODE_ENV === 'production') {
       console.log('Content has been cached for offline use.')
     },
     updatefound () {
-      console.log('New content is downloading.')
+      alert('New content is downloading.')
     },
     updated () {
-      console.log('New content is available; please refresh.')
+      alert('New content is available; please refresh.')
     },
     offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+      alert('No internet connection found. App is running in offline mode.')
     },
     error (error) {
       console.error('Error during service worker registration:', error)
